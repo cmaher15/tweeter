08e360809397c8617d7ada069453ad0c66331ef1
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-24 18:02:40 -0700

    added code to turn text red when it hits 0 or below

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-  $("textarea").on("keypress", onInput);
+  $("textarea").on("keyup", onInput);
 
 });
 
 const onInput = function() {
-  const textArea = $(this);
-  const length = textArea.val().length;
+  const $textArea = $(this);
+  const $length = 140 - $textArea.val().length;
 
   const $form = $(this).closest("form");
   const $counter = $form.find(".counter");
-  $counter.text(140 - length);
+  $counter.text($length);
+  if ($length <= 0) {
+    $counter.addClass("red-font");
+  }
 };
