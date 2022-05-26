db72164c7544333a4d6ae93c892e59a6c111e821
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-25 18:49:52 -0700

    added timeago to datestamp tweets the proper way

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,40 +8,18 @@
 
 // const text = require("body-parser/lib/types/text");
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1653166304245
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1653252704245
-//   }
-// ];
 
 $(document).ready(function() {
+  jQuery("time.timeago").timeago();
   const renderTweets = function(tweets) {
     for (let data of tweets) {
       const $newData = createTweetElement(data);
-      $('.tweets-container').append($newData);
+      $('.tweets-container').prepend($newData);
     }
   };
 
   const createTweetElement = function(tweet) {
+    const newDate = jQuery.timeago(new Date(tweet.created_at));
     const $tweet = (`
     <article class="tweets">
       <header>
@@ -51,7 +29,7 @@ $(document).ready(function() {
       </header>
       <div class="tweet-text">${tweet.content.text}</div>
       <footer>
-        ${tweet.created_at}
+        ${newDate}
         <p class="images"><i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
         <i class="fa-solid fa-heart"></i></p>
